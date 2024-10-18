9f2906625eb70ad1b730e3864abb5439e73d816d
Rafael Luna <work@rafaeluna.com>
2024-10-18 15:25:43 -0600

    Add ability to reset game on winscreen

--- stages/WinScreen.py
+++ stages/WinScreen.py
@@ -1,3 +1,5 @@
+import logging
+
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
@@ -5,6 +7,14 @@ from kivy.graphics import Color, Rectangle
 from kivy.uix.image import Image
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.app import App
+
+import config
+
+if config.IS_RPI:
+    import RPi.GPIO as GPIO
 
 class WinScreen(Screen):
     def __init__(self, name: str):
@@ -50,7 +60,32 @@ class WinScreen(Screen):
         bottom_text_layout.add_widget(bottom_text)
         self.add_widget(bottom_text_layout)
 
+        # Setup action to restart game
+        if config.IS_RPI:
+            GPIO.setmode(GPIO.BCM)
+            self.gpio_pin = config.GPIO_RESTART_PIN
+            GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            # Schedule the GPIO check
+            Clock.schedule_interval(self.check_gpio_input, 0.1)
+        else:
+            # If not on Raspberry Pi, use key event for simulation
+            Window.bind(on_key_down=self.on_key_down)
 
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+    def check_gpio_input(self, dt):
+        # If GPIO input detected (button pressed)
+        if GPIO.input(self.gpio_pin) == GPIO.LOW:
+            self.restart_full_game()
+
+    def on_key_down(self, window, key, scancode, codepoint, modifier):
+        # Listen for a specific key press (e.g., space bar, keycode 32)
+        if key == 114:  # Space key
+            self.restart_full_game()
+
+    def restart_full_game(self):
+        logging.debug("Trying to go to WelcomeScreen from WinScreen, current screen: %s", self.manager.current)
+        if (self.manager.current == self.name) or True:
+            logging.info("Restarting game from WinScreen (should go to WelcomeScreen)...")
+            app = App.get_running_app()
+            app.restart_music("bogus_arg")
+            app.reset_all_states()
+            self.manager.current = "welcome_screen"
